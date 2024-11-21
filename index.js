e6225b196087f10f373fd0a06c2513510a5dc8e7
SunnyZhang403 <sunnyzhang403@outlook.com>
2024-11-21 16:51:36 -0500

    fixed slideshows

--- index.js
+++ index.js
@@ -5,18 +5,26 @@ document.addEventListener("DOMContentLoaded", () => {
     const slides = slideshow.querySelectorAll("img");
     let currentSlide = 0;
 
+    // Show slide
     const showSlide = (index) => {
       slides.forEach((slide, i) => {
         slide.style.display = i === index ? "block" : "none";
       });
     };
 
-    const autoSwitch = () => {
+    // Automatic Slideshow
+    setInterval(() => {
       currentSlide = (currentSlide + 1) % slides.length;
       showSlide(currentSlide);
-    };
+    }, 5000); // 3 seconds per slide
 
+    // Manual Controls
+    slideshow.addEventListener("click", () => {
+      currentSlide = (currentSlide + 1) % slides.length;
+      showSlide(currentSlide);
+    });
+
+    // Initialize
     showSlide(currentSlide);
-    setInterval(autoSwitch, 3000);
   });
 });
